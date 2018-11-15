d6683e7400c5d4c31eee2fb43225f76e73bee5a1
Lars Palczak <lars@Larss-MacBook.fritz.box>
2018-11-15 22:30:52 +0100

    Solved assignment2 part 2 except for exercise 10

--- assignment_2/assignment2/svm_smo.py
+++ assignment_2/assignment2/svm_smo.py
@@ -77,14 +77,14 @@ def gaussian(x, y, sigma=1):
     # Compute the Gaussian kernel between x and y                         #
     #######################################################################
     
-    result = np.exp(-np.linalg.norm(x-y)**2/(2 * sigma**2))
+    #result = np.exp(-np.linalg.norm(x-y)**2/(2 * sigma**2))
     
-    #if np.ndim(x) == 1 and np.ndim(y) == 1:
-    #    result = np.exp(- np.linalg.norm(x - y) / (2 * sigma ** 2))
-    #elif (np.ndim(x) > 1 and np.ndim(y) == 1) or (np.ndim(x) == 1 and np.ndim(y) > 1):
-    #    result = np.exp(- np.linalg.norm(x - y, axis=1) / (2 * sigma ** 2))
-    #elif np.ndim(x) > 1 and np.ndim(y) > 1:
-    #    result = np.exp(- np.linalg.norm(x[:, np.newaxis] - y[np.newaxis, :], axis=2) / (2 * sigma ** 2))
+    if np.ndim(x) == 1 and np.ndim(y) == 1:
+        result = np.exp(- np.linalg.norm(x - y) / (2 * sigma ** 2))
+    elif (np.ndim(x) > 1 and np.ndim(y) == 1) or (np.ndim(x) == 1 and np.ndim(y) > 1):
+        result = np.exp(- np.linalg.norm(x - y, axis=1) / (2 * sigma ** 2))
+    elif np.ndim(x) > 1 and np.ndim(y) > 1:
+        result = np.exp(- np.linalg.norm(x[:, np.newaxis] - y[np.newaxis, :], axis=2) / (2 * sigma ** 2))
     
     #######################################################################
     #                         END OF YOUR CODE                            #
